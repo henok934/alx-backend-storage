8d2f9a09bc778693b4af63d9befcc0195d6ddb0b
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-08-10 08:32:42 -0400

    task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,8 @@
+
+-- File: create_users_table.sql
+-- Description: This script creates the 'users' table in the database.
+
+-- Create a new table called "users"
 CREATE TABLE IF NOT EXISTS users (
 	id INT, 
 	email VARCHAR(255),

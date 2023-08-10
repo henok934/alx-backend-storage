7f7c83cbbebeca0ccc43c8248a438ea172a0128d
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-08-10 13:32:32 -0400

    task2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,16 +1,5 @@
--- Create temporary table band_ranking
-CREATE TEMPORARY TABLE band_ranking (
-  origin VARCHAR(255),
-  nb_fans INT
-);
-
--- Insert data into band_ranking
-INSERT INTO band_ranking
+-- select data from metal_bands
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
-
--- Retrieve and display data from band_ranking
-SELECT origin, nb_fans
-FROM band_ranking;
